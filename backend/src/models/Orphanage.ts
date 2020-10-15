df4889ce9f2600961531a7629eed6a864339213e
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2020-10-15 00:12:02 -0300

    add controller

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -1,7 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity('orphanages')
-
 export default class Orphanage {
     @PrimaryGeneratedColumn('increment')
     id: number;
